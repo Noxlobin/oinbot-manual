b000386c19cf8be1d9fa03da30f9793373efcdab
Noxlobin <aadrijamaitydps@gmail.com>
2024-01-20 17:25:36 +0530

    finished app

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,64 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import step1 from "@/public/steps/step-1.png";
+import step2 from "@/public/steps/step-2.png";
+import step3 from "@/public/steps/step-3.png";
+import step4 from "@/public/steps/step-4.png";
+
+const steps = [
+  {
+    img: step1,
+    text: `First, go to <a href="https://discohook.org">https://discohook.org</a> and you will see a sight like the picture (minus the markings) beside. On the website you will see a box that I circled. Click on that box. Go to zemi&rsquo;s number and ask for a discord webhook. She will give it to you. Copy that message. Go back to the website. Click and hold on the box I told you to click on. If you see a paste option, click it. Once you see the text Zemi gave you appear, your&apos;e good to go.`,
+  },
+  {
+    img: step2,
+    text: `Next, to make your text not bloated, tap the crosses where it is written "embeds". If you see those big squares in the preview section gone, then you are ok. Otherwise, immeadiatly refresh the page. Start over from Step 1. You might mess something up.`,
+  },
+  {
+    img: step3,
+    text: "Now, below the text box I showed it step 1, you can write your message (aka the fun part) there. Click the box and start deleting whatevers in there. Do it till the end. Once it is plain simple and you can't delete anymore, start writing your message. It can be anything you wanted. However, I will be looking for a `hey` in the chat. So for your first message, write `hey` and DON'T CLICK ANYTHING ELSE. Wait till the last step to send a message. Zemi will immeadiatly remind you about this. ",
+  },
+  {
+    img: step4,
+    text: "This is the final step. Once you are fully ready to embrace the errors that will (probably) come, click the send button. This will send the message. This will send the message to the Discord server. If you get any errors, repeat from step 1 in a new tab. If you still get errors, just ignore them. We will get a notification and Zemi will inform you. Keep in mind you won&quot;t see the messages, so Zemi will show you the last five messages. Now you are able to chat on discord!!!",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="">
+      <header className=" bg-blue-600 text-white px-4 py-3 pb-5 text-center shadow-sm shadow-slate-500">
+        <p className="font-extrabold text-lg ">oinbot manual</p>
+        <p className="mt-1 font-semibold text-sm">text us</p>
+      </header>
+
+      <p className="m-6 w-fit">
+        This is the <span className="font-semibold">DISCORD OINBOT MANUAL</span>
+        !!! This is for you, Pinkie. If you are someone other than Pinkie or
+        Zemi, get outta here. You won&apos;t understand anything. I know you
+        cannot pass the captcha of making a discord account, so I made a website
+        dedicated to you bypassing one. This is a tutorial, so be greatful I
+        made one. I just want to say, unless you got a webhook fetish, we
+        won&apos;t help you with passing the captcha. I assume you are on
+        mobile. And discord, please don&apos;t sue us. Without futher ado, lets
+        make you a webhook:
+      </p>
+
+      <div className=" w-[70%] flex items-center flex-col space-y-10">
+        {steps.map((step) => (
+          <div className="flex space-x-2 h-96 w-[50.632rem] " key={step.text}>
+            <p
+              className="w-[90%] flex-shrink-0"
+              dangerouslySetInnerHTML={{ __html: step.text }}
+            ></p>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              alt="Image of the webhook link getting pasted "
+              src={step.img}
+            ></Image>
+          </div>
+        ))}
       </div>
     </main>
   );
